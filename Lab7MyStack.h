ce82b51705c7d44240cddb3a5723613cd9499359
mhanley99 <43153704+mhanley99@users.noreply.github.com>
2018-10-15 16:59:14 -0600

    Create Lab7MyStack.h

---
+++ Lab7MyStack.h
@@ -0,0 +1,23 @@
+// Matthew Hanley
+// Lab 5
+// COSC 2030
+// Lab7MyStack.h
+// function definitions
+
+#ifndef LAB7MS_H_
+#define LAB7MS_H_
+
+void stringstringReversal(string input)
+{
+
+}
+
+void stringstringReversal2(string input)
+{
+
+}
+
+
+
+
+#endif LAB7MS_H_
