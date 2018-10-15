c9dae3e3b73f5a86b28adf24e48c9af06f14ffd5
mhanley99 <43153704+mhanley99@users.noreply.github.com>
2018-10-15 16:59:42 -0600

    Create Lab7Driver.cpp

---
+++ Lab7Driver.cpp
@@ -0,0 +1,17 @@
+// Matthew Hanley
+// Lab 5
+// COSC 2030
+// Lab7Driver.cpp
+// main method
+
+using namespace std;
+#include <iostream>
+#include "Lab7MyStack.h"
+
+int main()
+{
+	
+	system("pause");
+	return 0;
+}
+
