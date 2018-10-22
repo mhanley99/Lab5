99d3f7bc8ddd40b552e332fcb743a93e16249ccc
mhanley99 <43153704+mhanley99@users.noreply.github.com>
2018-10-22 16:41:34 -0600

    Update Lab7MyStack.h

--- Lab7MyStack.h
+++ Lab7MyStack.h
@@ -7,7 +7,7 @@
 #ifndef LAB7MS_H_
 #define LAB7MS_H_
 
-void stringstringReversal(string input)
+void stringstringReversal1(string input)
 {
 
 }
@@ -17,7 +17,25 @@ void stringstringReversal2(string input)
 
 }
 
+void stringstringReversal3(string input)
+{
+	
+}
 
+void stringstringreversal4(string input)
+{
+
+}
+
+class mystack
+{
+public:
+	vector<char> input;
+	bool isEmpty() const;
+	void push(char& c);
+	char pull();
+
+};
 
 
 #endif LAB7MS_H_
