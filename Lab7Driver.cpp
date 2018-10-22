f868633ca4ce2e4e89185f30ae04da977f4412d9
mhanley99 <43153704+mhanley99@users.noreply.github.com>
2018-10-22 16:42:03 -0600

    Update Lab7Driver.cpp

--- Lab7Driver.cpp
+++ Lab7Driver.cpp
@@ -8,9 +8,29 @@ using namespace std;
 #include <iostream>
 #include "Lab7MyStack.h"
 
+using std::cout;
+
 int main()
 {
-	
+	string test1 = "hello world";
+	string test2 = "the sky is blue and the grass is green";
+	string test3 = "racecar";
+
+	stringstringReversal1(test1);
+	stringstringReversal2(test1);
+	stringstringReversal3(test1);
+	stringstringReversal4(test1);
+
+	stringstringReversal1(test2);
+	stringstringReversal2(test2);
+	stringstringReversal3(test2);
+	stringstringReversal4(test2);
+
+	stringstringReversal1(test3);
+	stringstringReversal2(test3);
+	stringstringReversal3(test3);
+	stringstringReversal4(test3);
+
 	system("pause");
 	return 0;
 }
