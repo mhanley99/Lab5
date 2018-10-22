8aec241124bffc2c00be23002560a7b14729da12
mhanley99 <43153704+mhanley99@users.noreply.github.com>
2018-10-22 16:42:41 -0600

    Update Lab7MyStack.cpp

--- Lab7MyStack.cpp
+++ Lab7MyStack.cpp
@@ -9,22 +9,23 @@ using namespace std;
 #include <stack>
 #include <vector>
 #include <string>
+#include <list>
 #include "Lab7MyStack.h"
 
 void stringstringReversal1(string &input)
 {
-	stack<char> mystack;
+	stack<char> thestack;
 	char buffer;
 	for (int index = 0; index < input.length(); index++)
 	{
 		buffer = input[index];
-		mystack.push(buffer);
+		thestack.push(buffer);
 	}
-	while (!mystack.empty())
+	int index = 0;
+	while (!thestack.empty())
 	{
-		int index = 0;
-		input[index] = mystack.top();
-		mystack.pop();
+		input[index] = thestack.top();
+		thestack.pop();
 		index++;
 	}
 }
@@ -38,11 +39,70 @@ void stringstringReversal2(string &input)
 		buffer = input[index];
 		myvec.push_back(buffer);
 	}
+	int index = 0;
 	while (!myvec.empty())
 	{
-		int index = 0;
 		input[index] = myvec.back();
 		myvec.pop_back();
 		index++;
+	}
+}
+
+void stringstringReversal3(string &input)
+{
+	list<char> mylist;
+	char buffer;
+	for (int index = 0; index < input.length; index++)
+	{
+		buffer = input[index];
+		mylist.push_back(buffer);
+	}
+	int index = 0;
+	while (!mylist.empty())
+	{
+		input[index] = mylist.back();
+		mylist.pop_back();
+		index++;
+	}
+}
+
+bool isEmpty(mystack &test)
+{
+	if (test.input.empty() == true)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void push(mystack &test, char &c)
+{
+	test.input.push_back(c);
+}
+
+char pull(mystack &test)
+{
+	char element;
+	element = test.input.back();
+	test.input.pop_back;
+	return element;
+}
+
+void stringstringReversal4(string &input)
+{
+	mystack thestack;
+	char buffer;
+	for (int index = 0; index < input.length; index++)
+	{
+		buffer = input[index];
+		thestack.push(buffer);
+	}
+	int index = 0;
+	while (!(thestack.isEmpty() == false))
+	{
+		input[index] = thestack.pull();
 	}
 }
